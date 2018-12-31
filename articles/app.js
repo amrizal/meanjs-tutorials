b9ec1ad0494faae79afe259038d9c96692fbd289
Amrizal <amrizal@outlook.com>
2018-12-31 17:27:54 +0800

    add mongoose to articles

--- articles/app.js
+++ articles/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+require('./models/articles');
+
+mongoose.connect('mongodb://localhost/articlesDB');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- articles/routes/api.js
+++ articles/routes/api.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Article = mongoose.model('Article');
+
 router.get('/articles', function(req, res, next) {
-  res.end ('Get details of all articles');
+  Article.find (function(err, articles){
+	  if (err){
+		  return res.send (500, err);
+	  }
+	  return res.send (articles);
+  });
 });
 
 router.get('/articles/:id', function(req, res, next) {
-  res.end ('Get details of article with id:' + req.params.id);
+  Article.findById(req.params.id, function(err, article){
+	  if(err)
+		  res.send(err);
+	  res.json(article);
+  });
 });
 
-router.post('/articles/:id', function(req, res, next) {
-  res.end ('Store details of article with id:' + req.params.id);
+router.post('/articles', function(req, res, next) {
+  var article = new Article();
+  article.username = req.body.username;
+  article.title = req.body.title;
+  article.text = req.body.text;
+  article.save(function (err, article){
+	  if (err){
+		  return res.send (500, err);
+	  }
+	  return res.json(article);
+  });
 });
 
-router.post('/articles/:id', function(req, res, next) {
-  res.end ('Update details of article with id:' + req.params.id);
-});
 
 module.exports = router;
