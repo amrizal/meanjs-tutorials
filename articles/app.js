57ec26674e58215145241cbecd507cb504c99861
Amrizal <amrizal@outlook.com>
2019-01-04 23:20:40 +0800

    Writing the Server-side Code for Authentication

--- articles/app.js
+++ articles/app.js
@@ -4,13 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
+
 require('./models/articles');
+require('./models/users');
 
 mongoose.connect('mongodb://localhost/articlesDB');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var api = require('./routes/api');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -19,14 +26,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(session({secret: 'keyboard cat'}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', api);
+app.use('/auth', authenticate);
+
+var initPassport = require('./passport-initialize');
+initPassport(passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
